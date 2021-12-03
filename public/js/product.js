b5cf9939f27bfb3efb221b4cc84684a557c112c5
cianncazar <cianncazar@gmail.com>
2021-12-03 10:30:47 +0800

    Inventory

---
+++ public/js/product.js
@@ -0,0 +1,40 @@
+function showCRUD(){
+    var d_crud = document.getElementById("display-crud");
+
+    if (d_crud.style.display === "block") {
+        d_crud.style.display = "none";
+    } 
+    else {
+        d_crud.style.display = "block";
+    }
+}
+
+
+//JavaScript function that enables or disables a submit button depending
+//on whether a checkbox has been ticked or not.
+function terms_changed(termsCheckBox){
+    var ellipsis = document.getElementsByClassName("ellipsis-icon");
+    var checkbox = document.getElementsByClassName("product-checkbox");
+    //If the checkbox has been checked
+    if(termsCheckBox.checked){
+        //Set the disabled property to FALSE and enable the button.
+        document.getElementById("select-multiple").disabled = false;
+        for (var i = 0, max = ellipsis.length; i < max; i++) {
+            ellipsis[i].style.display = "none";
+        }
+        for (var i = 0, max = checkbox.length; i < max; i++) {
+            checkbox[i].style.display = "block";
+        }
+        document.getElementById("select-multiple").style.filter = "opacity(100%)";
+    } else{
+        //Otherwise, disable the submit button.
+        document.getElementById("select-multiple").disabled = true;
+        for (var i = 0, max = ellipsis.length; i < max; i++) {
+            ellipsis[i].style.display = "block";
+        }
+        for (var i = 0, max = checkbox.length; i < max; i++) {
+            checkbox[i].style.display = "none";
+        }
+        document.getElementById("select-multiple").style.filter = "opacity(50%)";
+    }
+}
---
+++ storage/framework/views/c00c42c1bf8890d213d9475deed2f84435b2e35b.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Ciann Renel Cazar\Documents\GitHub\Mini_Sales_And_Inventory_Management_System\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
