9c85923f3fd7df087023aecbb84a7e618e01064e
cianncazar <cianncazar@gmail.com>
2021-12-10 18:30:27 +0800

    Fixed Search Bar and Placed other buttons

--- resources/views/inventory/inventory_search.blade.php
+++ resources/views/inventory/inventory_search.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.inventory_layout')
 @section('content')
-    <script src="js/product.js"></script>
+    <script src="../js/product.js"></script>
     <div class="container">
         <div class="d-md-flex justify-content-md-between mb-4">
             <h1>Sales and Inventory</h1>
-            <button type="button" class="primary-button px-5" onclick="location.href = 'inventory/create';">Add</button>
+            <button type="button" class="primary-button px-5" onclick="location.href = '../inventory/create';">Add</button>
         </div>
         <div class="input-group mb-2">
             <input type="search" class="form-control search-bar" placeholder="Search" aria-label="Search" id="product_search" name="product_search"
                 aria-describedby="search-addon" />
-            <button type="button" class="search-button px-4" onclick="location.href = '../inventory/search?product_search='+document.getElementByID('product_search').value;">search</button>
+            <button type="button" class="search-button px-4" onclick="location.href = '../../inventory/search?product_search='+document.getElementById('product_search').value;">search</button>
         </div>
         <div class="product-list">
             @foreach($product as $prod)
@@ -34,38 +34,17 @@
                             if (!e) var e = window.event;
                             e.cancelBubble = true;
                             if (e.stopPropagation) e.stopPropagation();
-                            location.href = 'inventory/delete/{{$prod -> product_id}}';">
+                            location.href = '../inventory/delete/{{$prod -> product_id}}';">
                         Delete</button>
                         <button type="button" class="primary-button px-3 ms-1 submit-button" onclick="
                             if (!e) var e = window.event;
                             e.cancelBubble = true;
                             if (e.stopPropagation) e.stopPropagation();
-                            location.href = 'inventory/edit/{{$prod -> product_id}}';">
+                            location.href = '../inventory/edit/{{$prod -> product_id}}';">
                         Update</button>
                     </div>
                 </div>
             @endforeach
-            <!-- <div class="product-row p-2 mb-1">
-                <div class="d-flex justify-content-around align-items-center">
-                    <img src="images/asus-rog.jpg" class="product-pic rounded-circle me-3">
-                    <div class="product-name-price">
-                        <p class="product-name">Asus ROG 5</p>
-                        <p class="product-price">P30,000</p>
-                        <p class="product-quantity">Stock: 2,000</p>
-                    </div>
-                    <div class="product-description me-3">
-                    Play to the max with ROG Phone 5, the gaming phone that takes no prisoners. Powered to win by the latest Qualcomm® Snapdragon™ 888 5G Mobile Platform, this futuristic wonder packs an unbelievably responsive 144 Hz / 1 ms display, a monster 6000 mAh1 battery system, massively upgraded AirTrigger 5 game controls, and our iconic GameFX audio system. ROG Phone 5 will take your gaming to a new dimension — if you dare.
-                    </div>
-                    <img src="images/ellipsis-circle-svgrepo-com.svg" class="ellipsis-icon" onclick="showCRUD()">
-                    <div class="product-checkbox"> 
-                        <input class="form-check-input" type="checkbox">
-                    </div>
-                </div>
-                <div class="crud-div mt-2" id="display-crud">
-                    <button type="button" class="secondary-button px-3 ms-1">Delete</button>
-                    <button type="button" class="primary-button px-3 ms-1">Update</button>
-                </div>
-            </div> -->
         </div>
         <div class="d-flex align-items-center mt-2">
             <div class="select-multiple-blue p-2 me-2">
