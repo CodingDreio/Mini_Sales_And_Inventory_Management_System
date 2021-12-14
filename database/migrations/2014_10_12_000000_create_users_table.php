343e3eae1e1adc2ba3cb09aedb030f360fd80b6e
Joseph Andrey Dacera <githubdreio@gmail.com>
2021-12-14 10:35:31 +0800

    removed unnecessary files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -28,7 +28,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            // $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
